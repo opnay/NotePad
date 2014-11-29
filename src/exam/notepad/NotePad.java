84d450224a9597a5d039e33517ca5de21b0f3420
OPNay <opnayn@gmail.com>
2014-11-29 17:33:41 +0900

    Add NotePad Constructor

--- src/exam/notepad/NotePad.java
+++ src/exam/notepad/NotePad.java
@@ -3,5 +3,13 @@ package exam.notepad;
 import javax.swing.JFrame;
 
 public class NotePad extends JFrame {
-
+	
+	public NotePad(String title) {
+		super(title);
+		
+		this.setSize(500,500);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
 }
