8ec21d7bb503683713fac8e021076f603f1ac5cb
OPNay <opnayn@gmail.com>
2014-11-29 17:41:10 +0900

    Add JMenuBar

--- src/exam/notepad/NotePad.java
+++ src/exam/notepad/NotePad.java
@@ -1,12 +1,28 @@
 package exam.notepad;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 
 public class NotePad extends JFrame {
+	
+	private JMenuBar menubar = new JMenuBar();
+	private JMenu fileMenu, editMenu, viewMenu, helpMenu;
 	
 	public NotePad(String title) {
 		super(title);
 		
+		fileMenu = new JMenu("파일");
+		editMenu = new JMenu("편집");
+		viewMenu = new JMenu("보기");
+		helpMenu = new JMenu("도움말");
+		
+		menubar.add(fileMenu);
+		menubar.add(editMenu);
+		menubar.add(viewMenu);
+		menubar.add(helpMenu);
+		
+		this.setJMenuBar(menubar);
 		this.setSize(500,500);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
